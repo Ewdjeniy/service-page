a11ff5406719d684f93947713227e66a8298d6ed
ewdjeniy <ewdjeniy@mail.ru>
2022-11-30 20:29:59 +0300

    repaire fonts

--- src/components/static.js
+++ src/components/static.js
@@ -1,82 +1,82 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import Slide from './slider/slider.js';
-//import WebDevelopment from './webdevelopment.js';
-//import People from './people.js';
-//import Navigation from './navigation.js';
-//import Query from './queries.js';
-//import Footer from './footer.js';
-//
-//class Static extends React.Component {
-//  render() {
-//    return (
-//      <div id="container">
-//        <section className="banner_bg">
-//          <Navigation />
-//          <Slide />
-//        </section>
-//        <section id="aboutUs"><img src={require("./images/aboutUsImg.jpg")}/>
-//          <article>
-//            <h2 className="A_little_about_us"> A LITTLE ABOUT US</h2>
-//            <div>
-//              <div className="Shape_2"></div>
-//              <div className="Shape_1"></div>
-//            </div>
-//            <div className="We_are__a_company_based_in_india_with_the_motto">
-//              <p>We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India".</p><br/>
-//              <p>We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India".</p><a className="READ_MORE_DETAILS" href="#">READ MORE DETAILS</a>
-//            </div>
-//          </article>
-//        </section>
-//        <section id="ourServices">
-//          <div className="leftLine">
-//            <div></div>
-//            <div></div>
-//          </div><span className="OUR_SERVICES">OUR SERVICES</span>
-//          <div className="rightLine">
-//            <div></div>
-//            <div></div>
-//          </div>
-//        </section>
-//        <WebDevelopment />
-//        <section id="Feedback">
-//          <div className="leftLine">
-//            <div className="Shape_4"></div>
-//            <div className="Shape_3"></div>
-//          </div><span className="Feedback">Feedback</span>
-//          <div className="rightLine">
-//            <div className="Shape_4"></div>
-//            <div className="Shape_3"></div>
-//          </div>
-//        </section>
-//        <People />
-//        <section>
-//          <div className="lineLeft">
-//            <div></div>
-//            <div></div>
-//          </div><span className="Clients"> Clients</span>
-//          <div className="lineRight">
-//            <div></div>
-//            <div></div>
-//          </div>
-//        </section>
-//        <section className="Sponsors">
-//          <div><img src={require("./images/Adobe.png")}/><img src={require("./images/HCL.png")}/><img src={require("./images/infosys-logo-PNG.png")}/><img src={require("./images/invision.png")}/></div>
-//          <div><img src={require("./images/cisco-logo.png")}/><img src={require("./images/be.png")}/><img src={require("./images/canva.png")}/><img src={require("./images/instagram.jpg")}/></div>
-//        </section>
-//        <section id="contacts">
-//          <div>
-//            <h2 className="Any_Queries___Get_a_quote"> Any Queries|Get a quote</h2>
-//            <p className="If_you_have_any_questions_or_queries_regarding_">If you have any questions or queries regarding our service or pricing don't worry Our experts will call You</p>
-//          </div>
-//          <Query />
-//        </section>
-//        <Footer />
-//      </div>
-//    );
-//  }
-//}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slide from './slider/slider.js';
+import WebDevelopment from './webdevelopment/webdevelopment.js';
+import People from './people/people.js';
+import Navigation from './navigation/navigation.js';
+import Query from './queries/queries.js';
+import Footer from './footer/footer.js';
+
+class Static extends React.Component {
+  render() {
+    return (
+      <div id="container">
+        <section className="banner_bg">
+          <Navigation />
+          <Slide />
+        </section>
+        <section id="aboutUs"><img src={require("./images/aboutUsImg.jpg")}/>
+          <article>
+            <h2 className="A_little_about_us"> A LITTLE ABOUT US</h2>
+            <div>
+              <div className="Shape_2"></div>
+              <div className="Shape_1"></div>
+            </div>
+            <div className="We_are__a_company_based_in_india_with_the_motto">
+              <p>We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India".</p><br/>
+              <p>We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India". We are a company based in India with the motto "Digitalally Designed India".</p><a className="READ_MORE_DETAILS" href="#">READ MORE DETAILS</a>
+            </div>
+          </article>
+        </section>
+        <section id="ourServices">
+          <div className="leftLine">
+            <div></div>
+            <div></div>
+          </div><span className="OUR_SERVICES">OUR SERVICES</span>
+          <div className="rightLine">
+            <div></div>
+            <div></div>
+          </div>
+        </section>
+        <WebDevelopment />
+        <section id="Feedback">
+          <div className="leftLine">
+            <div className="Shape_4"></div>
+            <div className="Shape_3"></div>
+          </div><span className="Feedback">Feedback</span>
+          <div className="rightLine">
+            <div className="Shape_4"></div>
+            <div className="Shape_3"></div>
+          </div>
+        </section>
+        <People />
+        <section>
+          <div className="lineLeft">
+            <div></div>
+            <div></div>
+          </div><span className="Clients"> Clients</span>
+          <div className="lineRight">
+            <div></div>
+            <div></div>
+          </div>
+        </section>
+        <section className="Sponsors">
+          <div><img src={require("./images/Adobe.png")}/><img src={require("./images/HCL.png")}/><img src={require("./images/infosys-logo-PNG.png")}/><img src={require("./images/invision.png")}/></div>
+          <div><img src={require("./images/cisco-logo.png")}/><img src={require("./images/be.png")}/><img src={require("./images/canva.png")}/><img src={require("./images/instagram.jpg")}/></div>
+        </section>
+        <section id="contacts">
+          <div>
+            <h2 className="Any_Queries___Get_a_quote"> Any Queries|Get a quote</h2>
+            <p className="If_you_have_any_questions_or_queries_regarding_">If you have any questions or queries regarding our service or pricing don't worry Our experts will call You</p>
+          </div>
+          <Query />
+        </section>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 
-//
-//export default Static;
+
+export default Static;
